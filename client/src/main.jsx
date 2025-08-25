11e184bc18a07e373b7448951aac5dc741600ec4
RuslanGon <ruslan441006@gmail.com>
2025-08-25 13:03:25 +0300

    wer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import 'normalize.css';
 import './index.css'
 import App from './App.jsx'
+import { store } from './redux/store.js';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <Provider store={store}>
+    <BrowserRouter>
     <App />
+  </BrowserRouter>
+  </Provider>
   </StrictMode>,
+  
 )
