78aa4762aaa1dedb702b06274d10a44c5f6f717f
RuslanGon <ruslan441006@gmail.com>
2025-08-25 12:12:19 +0300

    wer

---
+++ server/controllers/UserController.js
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// регистрация
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ name, email, passwordHash });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.status(201).json({
+      user: { id: newUser._id, name: newUser.name, email: newUser.email },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Registration error" });
+  }
+};
+
+// логин
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.json({
+      user: { id: user._id, name: user.name, email: user.email },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Login error" });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import startServer from './db.js'; 
+import { login, register } from './controllers/UserController.js';
 
 
 const app = express();
@@ -8,6 +9,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Регистрация, логин, получение своего профиля
+app.post("/auth/register", register);
+app.post("/auth/login", login);
 
 
 startServer(app);
