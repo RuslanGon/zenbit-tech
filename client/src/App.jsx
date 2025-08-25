39d1b61b80a3a366fb4ffe318dc371b800f78281
RuslanGon <ruslan441006@gmail.com>
2025-08-25 13:43:01 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register/Register.jsx";
 import Login from "./pages/Login/Login.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      {/* Позже добавим /applications */}
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        {/* Позже добавим /applications */}
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo}>Zenbit Tech</Link>
+      <nav>
+        {user ? (
+          <span>Привет, {user.name}</span>
+        ) : (
+          <>
+            <Link to="/login" className={styles.navLink}>Log In</Link>
+            <Link to="/register" className={styles.navLink}>Sign Up</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
